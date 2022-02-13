1a8f6d27947cd82eca251ec19d5d425e52f53606
Michael Bond <mbond@ltisolutions.com>
2022-02-13 13:14:26 -0500

    Add basic iCloud code

--- XamarinCloudTest/XamarinCloudTest.iOS/AppDelegate.cs
+++ XamarinCloudTest/XamarinCloudTest.iOS/AppDelegate.cs
@@ -1,4 +1,6 @@
-﻿using Foundation;
+﻿using System;
+using CloudKit;
+using Foundation;
 using UIKit;
 
 namespace XamarinCloudTest.iOS
@@ -9,6 +11,10 @@ namespace XamarinCloudTest.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        public override UIWindow Window { get; set; }
+        public CKDatabase PublicDatabase { get; set; }
+        public CKDatabase PrivateDatabase { get; set; }
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -18,10 +24,32 @@ namespace XamarinCloudTest.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            app.RegisterForRemoteNotifications();
+
+            // Get the default public and private databases for
+            // the application
+            PublicDatabase = CKContainer.DefaultContainer.PublicCloudDatabase;
+            PrivateDatabase = CKContainer.DefaultContainer.PrivateCloudDatabase;
+
             Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Console.WriteLine("Registered for Push notifications with token: {0}", deviceToken);
+        }
+
+        public override void FailedToRegisterForRemoteNotifications(UIApplication application, NSError error)
+        {
+            Console.WriteLine("Push subscription failed");
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            Console.WriteLine("Push received");
         }
     }
 }
--- XamarinCloudTest/XamarinCloudTest.iOS/DataService.cs
+++ XamarinCloudTest/XamarinCloudTest.iOS/DataService.cs
@@ -1,6 +1,66 @@
-﻿namespace XamarinCloudTest.iOS
+﻿using CloudKit;
+using Foundation;
+using UIKit;
+
+namespace XamarinCloudTest.iOS
 {
     public class DataService : IDataService
     {
+        public AppDelegate ThisApp => (AppDelegate)UIApplication.SharedApplication.Delegate;
+
+        private const string ReferenceItemRecordName = "ReferenceItems";
+
+        public void SaveRecord(string name)
+        {
+            // Create a new record
+            var newRecord = new CKRecord(ReferenceItemRecordName);
+            newRecord["name"] = (NSString)name;
+
+            // Save it to the database
+            ThisApp.PublicDatabase.SaveRecord(newRecord, (record, err) => {
+                // Was there an error?
+                if (err != null) {
+                }
+            });
+        }
+
+        public void FetchRecord(string myRecordName)
+        {
+            // Create a record ID and fetch the record from the database
+            var recordID = new CKRecordID(myRecordName);
+            ThisApp.PublicDatabase.FetchRecord(recordID, (record, err) => {
+                // Was there an error?
+                if (err != null) {
+                }
+            });
+        }
+
+        public void UpdateRecord(string myRecordName, string newName)
+        {
+            // Create a record ID and fetch the record from the database
+            var recordID = new CKRecordID(myRecordName);
+            ThisApp.PublicDatabase.FetchRecord(recordID, (record, err) => {
+                // Was there an error?
+                if (err != null) {
+
+                }
+                else {
+                    // Modify the record
+                    record["name"] = (NSString)newName;
+
+                    // Save changes to database
+                    ThisApp.PublicDatabase.SaveRecord(record, (r, e) => {
+                        // Was there an error?
+                        if (e != null) {
+                        }
+                    });
+                }
+            });
+        }
+
+        public void DeleteRecord(string myRecordName)
+        {
+
+        }
     }
 }
